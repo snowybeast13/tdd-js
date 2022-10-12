22c82c110ca2f9c09a74eb86b50847a152ca80e0
Uros Miric <roshula13@gmail.com>
2022-10-12 17:53:31 +0200

    adding test for functions and calculator functions

--- app.test.js
+++ app.test.js
@@ -1,3 +1,79 @@
-test('Jest is working', ()=>{
-    expect(1).toBe(1);
-})
+const calculator = require("./calculator");
+
+//Add test
+describe("Addition of two numbers", () => {
+  test("Addition of 10 and 3 to equal 13", () => {
+    expect(calculator.addCalculator(10, 3)).toBe(13);
+  });
+});
+
+//Substract test
+describe("Substraction of two numbers", () => {
+  test("Substraction of 10 and 3 to equal 7", () => {
+    expect(calculator.substractCalc(10, 3)).toBe(7);
+  });
+});
+
+//Multiplication test
+describe("Multiplication of two numbers", () => {
+  test("Multiplication of 3 and 7 to equal 21", () => {
+    expect(calculator.multiply(7, 3)).toBe(21);
+  });
+});
+
+//Dividing test
+describe("Devision of two numbers", () => {
+  test("Devision of 2424 and 6 to equal 404", () => {
+    expect(calculator.devide(2424, 6)).toBe(404);
+  });
+});
+
+//Calculation with more than 2 arguments
+describe("Calculation with more than two parameters", () => {
+  test("Sum 3 and 4 and multiply with 3 to equal 21", () => {
+    expect(calculator.complex(3, 4, 3)).toBe(21);
+  });
+});
+
+//Addition with only one parameter
+describe("Addition with only one parameter", () => {
+  test("Addition with one parameter", () => {
+    expect(() => calculator.addCalculator(3)).toThrow(
+      "Some parameters are missing"
+    );
+  });
+});
+
+//Addition with none of the parameters
+describe("Addition with none of parameters", () => {
+  test("Addition with none of parameters", () => {
+    expect(() => calculator.addCalculator()).toThrow(
+      "Some parameters are missing"
+    );
+  });
+});
+
+//Dividing with 0
+describe("Devision with 0", () => {
+  test("Devision of 5 with 0 to throw error", () => {
+    expect(() => calculator.devide(5, 0)).toThrow("Unable to devide with 0");
+  });
+});
+
+//Multiplication with string
+describe("Multiplication of two numbers if one is string", () => {
+  test("Multiplication of 3 and 7 but 7 is string", () => {
+    expect(() =>
+      calculator.multiply(3, "hello").toThrow("One of arguments are string")
+    );
+  });
+});
+
+//Array of numbers
+describe("Array of numbers", () => {
+  test("Array", () => {
+    expect(() =>
+      calculator.numbersOnly("Hello").toThrow("There's string in number array")
+    );
+  });
+});
---
+++ calculator.js
@@ -0,0 +1,65 @@
+//Add calculation
+const addCalculator = (num1, num2) => {
+  if (
+    num1 === undefined ||
+    num2 === undefined ||
+    (num1 === undefined && num2 === undefined)
+  ) {
+    throw new Error("Some parameters are missing");
+  } else return num1 + num2;
+};
+
+//Substract calculation
+const substractCalc = (num1, num2) => {
+  return num1 - num2;
+};
+
+//Multiply calculation
+const multiply = (num1, num2) => {
+  if (num1 === "" || num2 === "") {
+    throw new Error("One of arguments are string");
+  } else return num1 * num2;
+};
+
+//Devid calculation
+const devide = (num1, num2) => {
+  if (num2 === 0) {
+    throw new Error("Unable to devide with 0");
+  } else return num1 / num2;
+};
+
+//Complex calculation
+const complex = (num1, num2, num3) => {
+  return (num1 + num2) * num3;
+};
+
+//Array
+let arr = [7, "c1", "d2", 18, "dr", 21, 2];
+let numbers = arr.filter(numbersOnly);
+
+function numbersOnly(value) {
+  if (typeof value === "") {
+    throw Error("There's string in number array");
+  } else if (typeof value === "number") {
+    return value;
+  }
+}
+
+const arrayOfNumbers = (total, avarage, maxNum, minNum) => {
+  total = numbers.length;
+  avarage = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  console.log(avarage);
+  maxNum = Math.max(...numbers);
+  minNum = Math.min(...numbers);
+};
+
+
+//Exports
+module.exports = {
+  addCalculator,
+  substractCalc,
+  multiply,
+  devide,
+  complex,
+  numbersOnly,
+};
